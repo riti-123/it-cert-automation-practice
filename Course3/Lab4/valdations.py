ad9a8bdd30c819efa710858a2f53e8fb418fbc56
riti-123 <ritikamaarula21@gmail.com>
2020-07-30 11:37:37 +0000

    commit! closes: #1 update validations.py python script. fixed the behaviour of validate_user function in validations.py .

---
+++ Course3/Lab4/valdations.py
@@ -0,0 +1,3 @@
+There are lots of ways to fix the code; ypu will want to add additional conditional checks to validate the first charcarter doesn't start with either of the forbidden characters. you can choose whichever way you'd like to implement this.
+
+ 
